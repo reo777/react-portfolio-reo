1552bc01bf5a6f995628b34c6c1c75ccc8a144a3
reo777 <r.ishiyama@gemcook.com>
2019-03-18 23:26:31 +0900

    [add] ### initialcommit

---
+++ src/components/main/top.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from "react";
+
+class Top extends Component {
+  render() {
+    return (
+      <>
+        <h2>You dare or You don't</h2>
+      </>
+    );
+  }
+}
+
+export default Top;
