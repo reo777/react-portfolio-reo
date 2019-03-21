9177a8629c6d17657634ba3efeb98c07f8dae853
reo777 <r.ishiyama@gemcook.com>
2019-03-21 18:22:35 +0900

    [update] ### header

--- src/components/pages/Top/Main/index.jsx
+++ src/components/pages/Top/Main/index.jsx
@@ -1,10 +1,15 @@
 import React, {Component} from "react";
+import {Link} from "react-router-dom";
+import {paths} from "../../../../config/paths";
 
 class Top extends Component {
   render() {
     return (
       <>
         <h2>You dare or You don't</h2>
+        <h1>
+          <Link to={`${paths.about.main}`}>Who are you?</Link>
+        </h1>
       </>
     );
   }
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,26 @@
 import * as React from "react";
-import {Route, Switch} from "react-router-dom";
+import {Route, Switch, Link} from "react-router-dom";
 import {paths} from "../config/paths";
 import {Top} from "../components/pages/Top";
 import {Contact} from "../components/pages/Contact";
 import {About} from "../components/pages/About";
+import "./index.scss";
 
 function Routes() {
   return (
     <div className="common_routes">
+      <div className="main_header">
+        <div>
+          <Link to={`${paths.top.main}`}>Main</Link>
+        </div>
+        <div>
+          <Link to={`${paths.about.main}`}>About</Link>
+        </div>
+        <div>
+          <Link to={`${paths.contact.main}`}>Contact</Link>
+        </div>
+      </div>
+
       <Switch>
         <Route exact path={paths.top.main} component={Top} />
         <Route exact path={paths.about.main} component={About} />
